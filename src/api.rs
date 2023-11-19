27e43ee3f53ab47189ff7c3552e2434ed570acd2
Yuuki Toriyama <github@toriyama.dev>
2023-11-19 18:43:17 +0900

    parse()を定義

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,8 @@
-mod wasm;
+pub mod wasm;
 
 use crate::entity::{City, Prefecture};
 
-trait Api {
+pub trait Api {
     async fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, String>;
     async fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, String>;
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,30 @@
+use crate::api::Api;
+use crate::api::wasm::ApiImplForWasm;
+use crate::entity::ParsedAddress;
+use crate::parser::read_city::read_city;
+use crate::parser::read_prefecture::read_prefecture;
+use crate::parser::read_town::read_town;
+
 mod read_city;
-pub mod read_prefecture;
+mod read_prefecture;
 mod read_town;
+
+pub async fn parse(api: ApiImplForWasm, input: &str) -> ParsedAddress {
+    // 都道府県を特定
+    let (rest, prefecture_name) = read_prefecture(input).unwrap();
+    // その都道府県の市町村名リストを取得
+    let prefecture = api.get_prefecture_master(prefecture_name).await.unwrap();
+    // 市町村名を特定
+    let (rest, city_name) = read_city(rest, prefecture).unwrap();
+    // その市町村の町名リストを取得
+    let city = api.get_city_master(prefecture_name, city_name).await.unwrap();
+    // 町名を特定
+    let (rest, town_name) = read_town(rest, city).unwrap();
+
+    ParsedAddress {
+        prefecture: prefecture_name.to_string(),
+        city: city_name.to_string(),
+        town: town_name.to_string(),
+        rest: rest.to_string(),
+    }
+}
