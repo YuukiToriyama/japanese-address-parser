05737d3e8d2807f9adcdc8c8be98bb6bf6cd8eb2
Yuuki Toriyama <github@toriyama.dev>
2023-11-19 21:04:48 +0900

    `Parser.parse()`に対しテストコードを追加

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,20 @@ impl Parser {
         serde_json::to_string(&result).unwrap()
     }
 }
+
+#[cfg(test)]
+mod integration_tests {
+    use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+    use crate::Parser;
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    async fn parse_success() {
+        let parser = Parser();
+        assert_eq!(
+            parser.parse("岩手県盛岡市内丸10番1号").await,
+            "{\"prefecture\":\"岩手県\",\"city\":\"盛岡市\",\"town\":\"内丸\",\"rest\":\"10番1号\"}".to_string()
+        )
+    }
+}
