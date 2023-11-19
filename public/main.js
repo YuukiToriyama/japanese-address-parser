335067649f2e054bf6f83ccd95c1cd575a758530
Yuuki Toriyama <github@toriyama.dev>
2023-11-19 22:20:32 +0900

    wasmモジュールを動かすためのデモページを追加

---
+++ public/main.js
@@ -0,0 +1,15 @@
+import init, {Parser} from "../pkg/japanese_address_parser.js"
+
+const inputTextArea = document.getElementById("input")
+const outputTextArea = document.getElementById("output")
+
+init().then(() => {
+    document.getElementById("exec").addEventListener("click", () => {
+        const input = inputTextArea.value
+        alert("input: " + input)
+        const parser = new Parser()
+        parser.parse(input).then(result => {
+            outputTextArea.value = result
+        })
+    })
+})
