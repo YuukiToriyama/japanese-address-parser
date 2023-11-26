401c12e1f4d980e6c8210260e0bb0080f123a8d2
Yuuki Toriyama <github@toriyama.dev>
2023-11-26 17:00:21 +0900

    update: `get_prefecture_master()`でエラーが発生した場合、詳細なエラーを返すように修正

--- src/api/native.rs
+++ src/api/native.rs
@@ -1,20 +1,26 @@
 use crate::api::Api;
 use crate::entity::{City, Prefecture, Town};
+use crate::err::{ApiErrorKind, Error};
 
 pub struct ApiImplForNative {}
 
 impl Api for ApiImplForNative {
-    async fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, String> {
+    async fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error> {
         let endpoint = format!(
             "https://yuukitoriyama.github.io/geolonia-japanese-addresses-accompanist/{}/master.json",
             prefecture_name
         );
-        let response = reqwest::get(&endpoint).await.unwrap();
+        let response = match reqwest::get(&endpoint).await {
+            Ok(result) => result,
+            Err(_) => return Err(Error::new_api_error(ApiErrorKind::FETCH(endpoint))),
+        };
         if response.status() == 200 {
-            let prefecture = response.json::<Prefecture>().await.unwrap();
-            Ok(prefecture)
+            match response.json::<Prefecture>().await {
+                Ok(result) => Ok(result),
+                Err(_) => Err(Error::new_api_error(ApiErrorKind::DESERIALIZE(endpoint))),
+            }
         } else {
-            Err(format!("Failed to fetch {}", &endpoint))
+            Err(Error::new_api_error(ApiErrorKind::FETCH(endpoint)))
         }
     }
 
