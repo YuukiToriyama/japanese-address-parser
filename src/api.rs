cc9e350c7e802760e935afbb6b9821c24d5d300b
Yuuki Toriyama <github@toriyama.dev>
2023-12-02 23:05:47 +0900

    トレイト`BlockingApi`を定義
    Androidで動かす場合に用いる

--- src/api.rs
+++ src/api.rs
@@ -8,3 +8,8 @@ pub trait Api {
     async fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error>;
     async fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error>;
 }
+
+pub trait BlockingApi {
+    fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error>;
+    fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error>;
+}
