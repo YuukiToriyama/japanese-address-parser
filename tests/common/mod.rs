a061df89bdc699f6730c2ca864f26e3ad2d72721
Yuuki Toriyama <github@toriyama.dev>
2023-12-16 16:45:23 +0900

    update: 結合モジュールの改良: CSVからテストを生成し実行する部分を関数として切り出した

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,8 @@
 use csv::Reader;
 use serde::Deserialize;
 use std::fs::File;
+use std::panic;
+use japanese_address_parser::Parser;
 
 #[derive(Deserialize)]
 pub struct Record {
@@ -20,4 +22,34 @@ pub fn read_test_data_from_csv(file_path: &str) -> Result<Vec<Record>, &str> {
         records.push(record)
     }
     Ok(records)
+}
+
+pub async fn run_data_driven_tests(file_path: &str) {
+    let records = read_test_data_from_csv(file_path).unwrap();
+    let mut success_count = 0;
+    for record in &records {
+        let parser = Parser();
+        let result = parser.parse(&record.address).await;
+
+        let test_result = panic::catch_unwind(|| {
+            assert_eq!(result.address.prefecture, record.prefecture);
+            assert_eq!(result.address.city, record.city);
+            assert_eq!(result.address.town, record.town);
+            assert_eq!(result.address.rest, record.rest);
+        });
+        match test_result {
+            Ok(_) => {
+                println!("Success: {}", record.address);
+                success_count += 1;
+            }
+            Err(_) => println!("Failed: {}", record.address),
+        };
+    }
+    assert_eq!(
+        success_count,
+        records.len(),
+        "{} of {} cases failed.",
+        records.len() - success_count,
+        records.len()
+    );
 }
