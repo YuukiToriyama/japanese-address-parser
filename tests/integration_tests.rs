a89c0c17eb45db8ddca99e8eb43b87ccd9cf51c8
Yuuki Toriyama <github@toriyama.dev>
2023-12-17 20:20:03 +0900

    add: 異字体旧字体への対応: `異字体旧字体への対応テスト`を追加
    「薮」の表記ゆれの対応で追加しそびれたデータを追加

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -9,3 +9,8 @@ mod common;
 async fn 県庁所在地のデータテスト() {
     run_data_driven_tests("./tests/test_data/県庁所在地の住所データ.csv").await
 }
+
+#[tokio::test]
+async fn 異字体旧字体への対応テスト() {
+    run_data_driven_tests("./tests/test_data/異字体旧字体への対応.csv").await
+}
