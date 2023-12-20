364c651ffa6e57c70ab87818dc5d659efd20b6d7
Yuuki Toriyama <github@toriyama.dev>
2023-12-21 00:38:30 +0900

    update: デモページの改善: パース処理結果を表に表示するようにJavaScriptを修正

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 import init, {Parser} from "../pkg/japanese_address_parser.js"
 
 const inputTextArea = document.getElementById("input")
-const outputTextArea = document.getElementById("output")
 
 init().then(() => {
     document.getElementById("exec").addEventListener("click", () => {
@@ -9,7 +8,31 @@ init().then(() => {
         alert("input: " + input)
         const parser = new Parser()
         parser.parse(input).then(result => {
-            outputTextArea.value = JSON.stringify(result, null, "\t")
+            document.getElementById("result").appendChild(
+                createRow(input, result)
+            )
         })
     })
 })
+
+const createRow = (input, parseResult) => {
+    const tr = document.createElement("tr")
+    tr.appendChild(createCell(`<p>${input}</p>`))
+    if (parseResult.error === undefined) {
+        tr.appendChild(createCell("<p>成功</p>"))
+    } else {
+        tr.appendChild(createCell("<p>失敗</p>"))
+    }
+    tr.appendChild(createCell(`<p>${parseResult.address.prefecture}</p>`))
+    tr.appendChild(createCell(`<p>${parseResult.address.city}</p>`))
+    tr.appendChild(createCell(`<p>${parseResult.address.town}</p>`))
+    tr.appendChild(createCell(`<p>${parseResult.address.rest}</p>`))
+    tr.appendChild(createCell(`<code>${JSON.stringify(parseResult, null, null)}</code>`))
+    return tr
+}
+
+const createCell = (innerHtml) => {
+    const td = document.createElement("td")
+    td.innerHTML = innerHtml
+    return td
+}
