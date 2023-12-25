d380ba9981d898209e31751eff8d9d2eaf3c0aa2
Yuuki Toriyama <github@toriyama.dev>
2023-12-26 02:08:02 +0900

    add: ¥d丁目への対応: `NonKanjiBlockNumberFilter`を作成

--- src/parser/filter.rs
+++ src/parser/filter.rs
@@ -1,4 +1,5 @@
 pub mod fullwidth_character;
+pub mod non_kanji_block_number;
 
 pub trait Filter {
     fn apply(self, input: String) -> String;
---
+++ src/parser/filter/non_kanji_block_number.rs
@@ -0,0 +1,24 @@
+use regex::Regex;
+
+use crate::parser::filter::Filter;
+use crate::util::converter::JapaneseNumber;
+
+pub struct NonKanjiBlockNumberFilter {}
+
+impl Filter for NonKanjiBlockNumberFilter {
+    fn apply(self, input: String) -> String {
+        let expression = Regex::new(r"\D+(?<block_number>\d+)丁目").unwrap();
+        match expression.captures(&input) {
+            Some(captures) => {
+                let capture_block_number = &captures.name("block_number").unwrap().as_str();
+                let block_number = capture_block_number.parse::<i32>().unwrap();
+                input.replacen(
+                    capture_block_number,
+                    block_number.to_japanese_form().unwrap().as_str(),
+                    1,
+                )
+            }
+            None => input
+        }
+    }
+}
