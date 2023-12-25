3341858c1fdf769bfde778071f8e0606a146f94c
Yuuki Toriyama <github@toriyama.dev>
2023-12-26 01:03:46 +0900

    update: ¥d丁目への対応: 全角数字を半角数字に変換する関数を追加

--- src/util/converter.rs
+++ src/util/converter.rs
@@ -46,6 +46,22 @@ fn associate_arabic_number_to_japanese_number(input: i32) -> Option<&'static str
     }
 }
 
+pub fn convert_zenkaku_to_hankaku(c: char) -> char {
+    match c {
+        '０' => '0',
+        '１' => '1',
+        '２' => '2',
+        '３' => '3',
+        '４' => '4',
+        '５' => '5',
+        '６' => '6',
+        '７' => '7',
+        '８' => '8',
+        '９' => '9',
+        _ => c,
+    }
+}
+
 #[cfg(test)]
 mod japanese_number_converter_tests {
     use crate::util::converter::JapaneseNumber;
