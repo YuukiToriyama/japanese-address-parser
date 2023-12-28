f1047031c88722a4c7f13f5ff94f0767bb07245b
Yuuki Toriyama <github@toriyama.dev>
2023-12-28 15:10:53 +0900

    add: 正式でない住所表記への対応: 町名と番地を抽出する関数を定義

--- src/parser/filter/invalid_town_name_format.rs
+++ src/parser/filter/invalid_town_name_format.rs
@@ -1,5 +1,6 @@
 use crate::parser::filter::Filter;
 use crate::util::converter::JapaneseNumber;
+use regex::Regex;
 
 pub struct InvalidTownNameFormatFilter {}
 
@@ -7,6 +8,58 @@ impl Filter for InvalidTownNameFormatFilter {
     fn apply(self, input: String) -> String {
         todo!()
     }
+}
+
+fn extract_town_name(input: &String) -> Option<(String, String)> {
+    let expression = Regex::new(
+        r"^(?<town_name>\D+)(?<block_number>\d+)(?<rest>.*)$"
+    ).unwrap();
+    let captures = expression.captures(input).unwrap();
+    let town_name = if let Some(matched) = captures.name("town_name") {
+        matched.as_str()
+    } else {
+        return None;
+    };
+    let block_number = if let Some(matched) = captures.name("block_number") {
+        matched.as_str().parse::<i32>().unwrap().to_japanese_form().unwrap()
+    } else {
+        return None;
+    };
+    let rest = if let Some(matched) = captures.name("rest") {
+        matched.as_str()
+    } else {
+        ""
+    };
+    Some((format!(
+        "{}{}丁目",
+        town_name,
+        block_number
+    ), rest.to_string()))
+}
+
+fn extract_house_number(input: &String) -> Option<(String, String)> {
+    let expression = Regex::new(
+        r"\D+(?<house_number>\d+)\D*(?<rest>.*)$"
+    ).unwrap();
+    let captures = if let Some(captures) = expression.captures(input) {
+        captures
+    } else {
+        return None;
+    };
+    let house_number = if let Some(matched) = captures.name("house_number") {
+        matched.as_str()
+    } else {
+        return None;
+    };
+    let rest = if let Some(matched) = captures.name("rest") {
+        matched.as_str()
+    } else {
+        ""
+    };
+    Some((format!(
+        "{}番",
+        house_number,
+    ), rest.to_string()))
 }
 
 #[cfg(test)]
