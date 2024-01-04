46b6aba875879630e374a0b12cc745cd61681b6f
Yuuki Toriyama <github@toriyama.dev>
2024-01-04 23:37:00 +0900

    update: parserモジュールのエンハンス: `read_city()`に対するテストをオフラインテストからオンラインテストに変更

--- src/parser/read_city.rs
+++ src/parser/read_city.rs
@@ -24,14 +24,14 @@ pub fn read_city(input: &str, prefecture: Prefecture) -> Option<(String, String)
 #[cfg(test)]
 mod tests {
     use crate::entity::Prefecture;
+    use crate::api::blocking::Client;
+    use crate::api::BlockingApi;
     use crate::parser::read_city::read_city;
 
     #[test]
     fn 京都市山科区_成功() {
-        let prefecture = Prefecture::new(
-            "京都府",
-            vec!["京都市北区", "京都市上京区", "京都市山科区", "京都市西京区"],
-        );
+        let api = Client {};
+        let prefecture = api.get_prefecture_master("京都府").unwrap();
         let (rest, city) = read_city("京都市山科区椥辻池尻町14-2", prefecture).unwrap();
         assert_eq!(rest, "椥辻池尻町14-2");
         assert_eq!(city, "京都市山科区");
@@ -39,19 +39,15 @@ mod tests {
 
     #[test]
     fn 市区町村名が誤っている_失敗() {
-        let prefecture = Prefecture::new(
-            "京都府",
-            vec!["京都市北区", "京都市上京区", "京都市山科区", "京都市西京区"],
-        );
+        let api = Client {};
+        let prefecture = api.get_prefecture_master("京都府").unwrap();
         assert_eq!(read_city("港区芝公園4丁目2-8", prefecture), None);
     }
 
     #[test]
     fn 表記ゆれ_茅ヶ崎市() {
-        let prefecture = Prefecture::new(
-            "神奈川県",
-            vec!["鎌倉市", "藤沢市", "小田原市", "茅ヶ崎市", "逗子市"],
-        );
+        let api = Client {};
+        let prefecture = api.get_prefecture_master("神奈川県").unwrap();
         let (rest, city) = read_city("茅ケ崎市香川5丁目1", prefecture).unwrap();
         assert_eq!(rest, "香川5丁目1");
         assert_eq!(city, "茅ヶ崎市");
@@ -59,15 +55,8 @@ mod tests {
 
     #[test]
     fn 表記ゆれ_横浜市保土ケ谷区() {
-        let prefecture = Prefecture::new(
-            "神奈川県",
-            vec![
-                "横浜市中区",
-                "横浜市南区",
-                "横浜市保土ケ谷区",
-                "横浜市磯子区",
-            ],
-        );
+        let api = Client {};
+        let prefecture = api.get_prefecture_master("神奈川県").unwrap();
         let (rest, city) = read_city("横浜市保土ヶ谷区川辺町2番地9", prefecture).unwrap();
         assert_eq!(rest, "川辺町2番地9");
         assert_eq!(city, "横浜市保土ケ谷区");
@@ -75,16 +64,8 @@ mod tests {
 
     #[test]
     fn 表記ゆれ_不破郡関ケ原町() {
-        let prefecture = Prefecture::new(
-            "岐阜県",
-            vec![
-                "養老郡養老町",
-                "不破郡垂井町",
-                "不破郡関ケ原町",
-                "安八郡神戸町",
-                "安八郡輪之内町",
-            ],
-        );
+        let api = Client {};
+        let prefecture = api.get_prefecture_master("岐阜県").unwrap();
         let (rest, city) = read_city("不破郡関が原町大字関ケ原894番地の58", prefecture).unwrap();
         assert_eq!(rest, "大字関ケ原894番地の58");
         assert_eq!(city, "不破郡関ケ原町");
