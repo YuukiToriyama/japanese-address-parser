cd452fb3f007dbc6eaae599db0f50c5064f5445f
Yuuki Toriyama <github@toriyama.dev>
2024-01-07 21:56:30 +0900

    update: API失敗時のテストコード: `CityMasterApi`に対するテストコードを追加
    `client.rs`, `blocking.rs`にそれぞれ書いていたものを移植している

--- src/api/city_master_api.rs
+++ src/api/city_master_api.rs
@@ -44,3 +44,77 @@ impl CityMasterApi {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::api::city_master_api::CityMasterApi;
+    use crate::entity::Town;
+
+    #[tokio::test]
+    async fn 非同期_石川県羽咋郡志賀町_成功() {
+        let city_master_api = CityMasterApi {
+            server_url: "https://geolonia.github.io/japanese-addresses/api/ja",
+        };
+        let result = city_master_api.get("石川県", "羽咋郡志賀町").await;
+        let city = result.unwrap();
+        assert_eq!(city.name, "羽咋郡志賀町");
+        let town = Town {
+            name: "末吉".to_string(),
+            koaza: "千古".to_string(),
+            lat: Some(37.006235),
+            lng: Some(136.779155),
+        };
+        assert!(city.towns.contains(&town));
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    #[test]
+    fn 同期_石川県羽咋郡志賀町_成功() {
+        let city_master_api = CityMasterApi {
+            server_url: "https://geolonia.github.io/japanese-addresses/api/ja",
+        };
+        let result = city_master_api.get_blocking("石川県", "羽咋郡志賀町");
+        let city = result.unwrap();
+        assert_eq!(city.name, "羽咋郡志賀町");
+        let town = Town {
+            name: "末吉".to_string(),
+            koaza: "千古".to_string(),
+            lat: Some(37.006235),
+            lng: Some(136.779155),
+        };
+        assert!(city.towns.contains(&town));
+    }
+
+    #[tokio::test]
+    async fn 非同期_誤った市区町村名_失敗() {
+        let city_master_api = CityMasterApi {
+            server_url: "https://geolonia.github.io/japanese-addresses/api/ja",
+        };
+        let result = city_master_api.get("石川県", "敦賀市").await;
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap().error_message,
+            format!(
+                "{}/石川県/敦賀市.jsonを取得できませんでした",
+                city_master_api.server_url
+            )
+        );
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    #[test]
+    fn 同期_誤った市区町村名_失敗() {
+        let city_master_api = CityMasterApi {
+            server_url: "https://geolonia.github.io/japanese-addresses/api/ja",
+        };
+        let result = city_master_api.get_blocking("石川県", "敦賀市");
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap().error_message,
+            format!(
+                "{}/石川県/敦賀市.jsonを取得できませんでした",
+                city_master_api.server_url
+            )
+        );
+    }
+}
