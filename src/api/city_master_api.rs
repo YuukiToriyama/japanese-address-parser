8a9f54975b39756de78d4a09c3d8e485e43ccf9d
Yuuki Toriyama <github@toriyama.dev>
2024-01-07 21:22:37 +0900

    update: API失敗時のテストコード: `CityMasterApi.get()`を実装

--- src/api/city_master_api.rs
+++ src/api/city_master_api.rs
@@ -1,5 +1,5 @@
-use crate::entity::City;
-use crate::err::Error;
+use crate::entity::{City, Town};
+use crate::err::{ApiErrorKind, Error};
 
 pub struct CityMasterApi {
     server_url: &'static str,
@@ -7,7 +7,22 @@ pub struct CityMasterApi {
 
 impl CityMasterApi {
     async fn get(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error> {
-        todo!()
+        let endpoint = format!("{}/{}/{}.json", self.server_url, prefecture_name, city_name);
+        let response = match reqwest::get(&endpoint).await {
+            Ok(result) => result,
+            Err(_) => return Err(Error::new_api_error(ApiErrorKind::Deserialize(endpoint))),
+        };
+        if response.status() == 200 {
+            match response.json::<Vec<Town>>().await {
+                Ok(result) => Ok(City {
+                    name: city_name.to_string(),
+                    towns: result,
+                }),
+                Err(_) => Err(Error::new_api_error(ApiErrorKind::Deserialize(endpoint))),
+            }
+        } else {
+            Err(Error::new_api_error(ApiErrorKind::Fetch(endpoint)))
+        }
     }
     #[cfg(not(target_arch = "wasm32"))]
     fn get_blocking(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error> {
