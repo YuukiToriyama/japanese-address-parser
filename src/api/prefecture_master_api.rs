ab5a4533c3c3b249663b0b5a002d690f81e24e5e
Yuuki Toriyama <github@toriyama.dev>
2024-01-07 21:11:36 +0900

    update: API失敗時のテストコード: `PrefectureMasterApi.get()`を実装

--- src/api/prefecture_master_api.rs
+++ src/api/prefecture_master_api.rs
@@ -1,11 +1,25 @@
 use crate::entity::Prefecture;
-use crate::err::Error;
+use crate::err::{ApiErrorKind, Error};
 
-pub struct PrefectureMasterApi {}
+pub struct PrefectureMasterApi {
+    server_url: &'static str,
+}
 
 impl PrefectureMasterApi {
     async fn get(&self, prefecture_name: &str) -> Result<Prefecture, Error> {
-        todo!()
+        let endpoint = format!("{}/{}/master.json", self.server_url, prefecture_name);
+        let response = match reqwest::get(&endpoint).await {
+            Ok(result) => result,
+            Err(_) => return Err(Error::new_api_error(ApiErrorKind::Fetch(endpoint))),
+        };
+        if response.status() == 200 {
+            match response.json::<Prefecture>().await {
+                Ok(result) => Ok(result),
+                Err(_) => Err(Error::new_api_error(ApiErrorKind::Deserialize(endpoint))),
+            }
+        } else {
+            Err(Error::new_api_error(ApiErrorKind::Fetch(endpoint)))
+        }
     }
     #[cfg(not(target_arch = "wasm32"))]
     fn get_blocking(&self, prefecture_name: &str) -> Result<Prefecture, Error> {
