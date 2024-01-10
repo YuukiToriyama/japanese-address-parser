ad12d5e1a5b76c5fcef0d743adc5f43fb3c067b9
Yuuki Toriyama <github@toriyama.dev>
2024-01-11 00:24:26 +0900

    add: API失敗時のテストコード: `api::ApiImpl`を実装

--- src/api.rs
+++ src/api.rs
@@ -4,6 +4,8 @@ pub mod city_master_api;
 pub mod client;
 pub mod prefecture_master_api;
 
+use crate::api::city_master_api::CityMasterApi;
+use crate::api::prefecture_master_api::PrefectureMasterApi;
 use crate::entity::{City, Prefecture};
 use crate::err::Error;
 use std::future::Future;
@@ -18,6 +20,31 @@ pub trait Api {
         prefecture_name: &str,
         city_name: &str,
     ) -> impl Future<Output = Result<City, Error>>;
+}
+
+pub struct ApiImpl {}
+
+impl Api for ApiImpl {
+    fn get_prefecture_master(
+        &self,
+        prefecture_name: &str,
+    ) -> impl Future<Output = Result<Prefecture, Error>> {
+        let prefecture_master_api = PrefectureMasterApi {
+            server_url: "https://yuukitoriyama.github.io/geolonia-japanese-addresses-accompanist",
+        };
+        prefecture_master_api.get(prefecture_name)
+    }
+
+    fn get_city_master(
+        &self,
+        prefecture_name: &str,
+        city_name: &str,
+    ) -> impl Future<Output = Result<City, Error>> {
+        let city_master_api = CityMasterApi {
+            server_url: "https://geolonia.github.io/japanese-addresses/api/ja",
+        };
+        city_master_api.get(prefecture_name, city_name)
+    }
 }
 
 pub trait BlockingApi {
