10d9aa720bdc35dda6c75730d6326d2504cbedd7
Yuuki Toriyama <github@toriyama.dev>
2024-01-11 00:27:25 +0900

    add: API失敗時のテストコード: `api::BlockingApiImpl`を実装

--- src/api.rs
+++ src/api.rs
@@ -47,7 +47,28 @@ impl Api for ApiImpl {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 pub trait BlockingApi {
     fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error>;
     fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error>;
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub struct BlockingApiImpl {}
+
+#[cfg(not(target_arch = "wasm32"))]
+impl BlockingApi for BlockingApiImpl {
+    fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error> {
+        let prefecture_master_api = PrefectureMasterApi {
+            server_url: "https://yuukitoriyama.github.io/geolonia-japanese-addresses-accompanist",
+        };
+        prefecture_master_api.get_blocking(prefecture_name)
+    }
+
+    fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error> {
+        let city_master_api = CityMasterApi {
+            server_url: "https://geolonia.github.io/japanese-addresses/api/ja",
+        };
+        city_master_api.get_blocking(prefecture_name, city_name)
+    }
 }
