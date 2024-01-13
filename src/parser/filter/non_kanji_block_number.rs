e94ce4046f044cfdbf3882063bb5339e8c8bfa0a
Yuuki Toriyama <github@toriyama.dev>
2024-01-13 13:35:34 +0900

    update: JS側のRegExpを使用する: `filter_with_js_sys_regexp()`を定義

--- src/parser/filter/non_kanji_block_number.rs
+++ src/parser/filter/non_kanji_block_number.rs
@@ -24,3 +24,19 @@ fn filter_with_regex(input: String) -> String {
         None => input,
     }
 }
+
+fn filter_with_js_sys_regexp(input: String) -> String {
+    let expression = js_sys::RegExp::new(r"\D+(?<block_number>\d+)丁目", "");
+    match expression.exec(&input) {
+        Some(result) => {
+            let capture_block_number = result.get(1).as_string().unwrap();
+            let block_number = capture_block_number.parse::<i32>().unwrap();
+            input.replacen(
+                &capture_block_number,
+                block_number.to_japanese_form().unwrap().as_str(),
+                1,
+            )
+        }
+        None => input,
+    }
+}
