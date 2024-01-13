50eea0ca3d16fecb864d1c4de45165785876f4a8
Yuuki Toriyama <github@toriyama.dev>
2024-01-13 17:20:32 +0900

    update: JS側のRegExpを使用する: `read_house_number_with_js_sys_regexp()`を定義

--- src/parser/read_house_number.rs
+++ src/parser/read_house_number.rs
@@ -15,6 +15,18 @@ fn read_house_number_with_regex(input: &str) -> Option<(String, String)> {
     Some((rest.to_string(), format!("{}番", house_number)))
 }
 
+#[allow(dead_code)]
+fn read_house_number_with_js_sys_regexp(input: &str) -> Option<(String, String)> {
+    let expression = js_sys::RegExp::new(r"(?<house_number>\d+)\D*(?<rest>.*)$", "");
+    let captures = expression.exec(input)?;
+    let house_number = captures.get(1).as_string()?;
+    let rest = captures
+        .get(2)
+        .as_string()
+        .unwrap_or_else(|| "".to_string());
+    Some((rest, format!("{}番", house_number)))
+}
+
 #[cfg(test)]
 mod tests {
     use crate::parser::read_house_number::read_house_number_with_regex;
