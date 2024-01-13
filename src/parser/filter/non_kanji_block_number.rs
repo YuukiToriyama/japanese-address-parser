7114ecc565b0db6ae49353d9d2fcce79db8f932b
Yuuki Toriyama <github@toriyama.dev>
2024-01-13 13:31:56 +0900

    update: JS側のRegExpを使用する: `NonKanjiBlockNumberFilter.apply()`の中身を関数に切り出した

--- src/parser/filter/non_kanji_block_number.rs
+++ src/parser/filter/non_kanji_block_number.rs
@@ -5,18 +5,22 @@ pub struct NonKanjiBlockNumberFilter {}
 
 impl Filter for NonKanjiBlockNumberFilter {
     fn apply(self, input: String) -> String {
-        let expression = regex::Regex::new(r"\D+(?<block_number>\d+)丁目").unwrap();
-        match expression.captures(&input) {
-            Some(captures) => {
-                let capture_block_number = &captures.name("block_number").unwrap().as_str();
-                let block_number = capture_block_number.parse::<i32>().unwrap();
-                input.replacen(
-                    capture_block_number,
-                    block_number.to_japanese_form().unwrap().as_str(),
-                    1,
-                )
-            }
-            None => input,
+        filter_with_regex(input)
+    }
+}
+
+fn filter_with_regex(input: String) -> String {
+    let expression = regex::Regex::new(r"\D+(?<block_number>\d+)丁目").unwrap();
+    match expression.captures(&input) {
+        Some(captures) => {
+            let capture_block_number = &captures.name("block_number").unwrap().as_str();
+            let block_number = capture_block_number.parse::<i32>().unwrap();
+            input.replacen(
+                capture_block_number,
+                block_number.to_japanese_form().unwrap().as_str(),
+                1,
+            )
         }
+        None => input,
     }
 }
