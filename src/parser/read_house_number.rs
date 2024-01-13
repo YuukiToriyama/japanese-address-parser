b7f3596157095cb03270ae7cd46e6343cda07c83
Yuuki Toriyama <github@toriyama.dev>
2024-01-13 17:00:28 +0900

    rename: JS側のRegExpを使用する: `read_house_number()` -> `read_house_number_with_regex()`

--- src/parser/read_house_number.rs
+++ src/parser/read_house_number.rs
@@ -1,8 +1,6 @@
-use regex::Regex;
-
 #[allow(dead_code)]
-pub fn read_house_number(input: &str) -> Option<(String, String)> {
-    let expression = Regex::new(r"(?<house_number>\d+)\D*(?<rest>.*)$").unwrap();
+pub fn read_house_number_with_regex(input: &str) -> Option<(String, String)> {
+    let expression = regex::Regex::new(r"(?<house_number>\d+)\D*(?<rest>.*)$").unwrap();
     let captures = expression.captures(input)?;
     let house_number = if let Some(matched) = captures.name("house_number") {
         matched.as_str()
@@ -19,18 +17,18 @@ pub fn read_house_number(input: &str) -> Option<(String, String)> {
 
 #[cfg(test)]
 mod read_house_number_tests {
-    use crate::parser::read_house_number::read_house_number;
+    use crate::parser::read_house_number::read_house_number_with_regex;
 
     #[test]
     fn read_house_number_1番() {
-        let (rest, house_number) = read_house_number("1").unwrap();
+        let (rest, house_number) = read_house_number_with_regex("1").unwrap();
         assert_eq!(house_number, "1番");
         assert_eq!(rest, "");
     }
 
     #[test]
     fn read_house_number_3番2() {
-        let (rest, house_number) = read_house_number("3-2").unwrap();
+        let (rest, house_number) = read_house_number_with_regex("3-2").unwrap();
         assert_eq!(house_number, "3番");
         assert_eq!(rest, "2");
     }
