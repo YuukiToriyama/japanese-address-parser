2315c89b5524d4409038a083451b68a04f85c21f
Yuuki Toriyama <github@toriyama.dev>
2024-02-28 23:53:46 +0900

    update: ワークスペースの導入: ParseResultをJsValueに変換するために定義している関数にコメントを追加

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,6 +7,7 @@ mod err;
 pub mod parser;
 mod util;
 
+// TODO: wasmクレートをビルドするのに必要なため残してあるが、削除できるならば削除したい
 impl From<ParseResult> for JsValue {
     fn from(value: ParseResult) -> Self {
         serde_wasm_bindgen::to_value(&value).unwrap()
