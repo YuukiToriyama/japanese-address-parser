4e29ec91b823bf3b73be960216bb93180676903b
Yuuki Toriyama <github@toriyama.dev>
2024-03-02 21:45:47 +0900

    remove: tsifyの除却: `Parser`に対する単体テストを削除
    `Parser.parse()`の返り値の型が変わったことでテストが通らなくなったため

--- src/lib.rs
+++ src/lib.rs
@@ -59,34 +59,3 @@ impl From<ParseResult> for JsValue {
         serde_wasm_bindgen::to_value(&value).unwrap()
     }
 }
-
-#[cfg(test)]
-mod parser_tests {
-    use crate::entity::{Address, ParseResult};
-    use crate::err::{Error, ParseErrorKind};
-    use crate::Parser;
-
-    #[tokio::test]
-    async fn parse_成功_実在する住所() {
-        let parser = Parser();
-        assert_eq!(
-            parser.parse("岩手県盛岡市内丸10番1号").await,
-            ParseResult {
-                address: Address::new("岩手県", "盛岡市", "内丸", "10番1号"),
-                error: None,
-            }
-        )
-    }
-
-    #[tokio::test]
-    async fn parse_失敗_実在しない町名() {
-        let parser = Parser();
-        assert_eq!(
-            parser.parse("東京都中央区銀座九丁目").await,
-            ParseResult {
-                address: Address::new("東京都", "中央区", "", "銀座九丁目"),
-                error: Some(Error::new_parse_error(ParseErrorKind::Town)),
-            }
-        )
-    }
-}
