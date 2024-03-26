66702ac6b4ec2b99de9c6fe74a3c219cda0b18cd
Yuuki Toriyama <github@toriyama.dev>
2024-03-27 02:40:52 +0900

    update: pythonモジュール: `ParseResult`から`PyParseResult`に変換するための処理を実装

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,3 +1,4 @@
+use japanese_address_parser::entity::ParseResult;
 use pyo3::prelude::*;
 use std::collections::HashMap;
 
@@ -7,6 +8,22 @@ struct PyParseResult {
     address: HashMap<String, String>,
     #[pyo3(get)]
     error: HashMap<String, String>,
+}
+
+impl From<ParseResult> for PyParseResult {
+    fn from(value: ParseResult) -> Self {
+        let mut address = HashMap::new();
+        address.insert(String::from("prefecture"), value.address.prefecture);
+        address.insert(String::from("city"), value.address.city);
+        address.insert(String::from("town"), value.address.town);
+        address.insert(String::from("rest"), value.address.rest);
+        let mut error = HashMap::new();
+        if let Some(err) = value.error {
+            error.insert(String::from("error_type"), err.error_type);
+            error.insert(String::from("error_message"), err.error_message);
+        }
+        Self { address, error }
+    }
 }
 
 #[pymodule]
