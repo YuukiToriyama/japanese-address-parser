10b0a7cb7232c260e5674c3f6343de2eebcfd106
Yuuki Toriyama <github@toriyama.dev>
2024-04-10 23:19:46 +0900

    add: pyiファイルの追加: `japanese_address_parser_py.pyi`を追加
    Rustから生成されるPythonバインディングに対する型定義を記したファイル

---
+++ python/japanese_address_parser_py.pyi
@@ -0,0 +1,31 @@
+class ParseResult:
+    """
+    A class represent parse result.
+
+    パース処理の結果を表すクラスです。
+    """
+
+    address: dict[str, str]
+    """
+    都道府県名、市区町村名、町名、それ以降の文字列をそれぞれ格納する辞書型を返します。
+    
+    {prefecture: str, city: str, town: str, rest: str}
+    """
+
+    error: dict[str, str]
+    """
+    パース処理中にエラーが発生した場合、エラーのタイプとエラーメッセージを格納する辞書型を返します。
+    
+    {error_type: str, error_message: str}
+    """
+
+
+def parse(address: str) -> ParseResult:
+    """
+    Format informal address into formal style
+
+    入力された住所を正式な表記に整形します。
+
+    :param address: 住所
+    :return: ParseResult
+    """
