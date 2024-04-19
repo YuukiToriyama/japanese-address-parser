15a14548502ebab199cf50bebdef3a614ed39f50
Yuuki Toriyama <github@toriyama.dev>
2024-04-20 00:58:52 +0900

    update: 郡の省略への対応: `VagueExpressionAdapter.apply()`内のmatch式をif letで書き換えた

--- core/src/parser/adapter/vague_expression_adapter.rs
+++ core/src/parser/adapter/vague_expression_adapter.rs
@@ -8,23 +8,19 @@ pub struct VagueExpressionAdapter;
 
 impl VagueExpressionAdapter {
     pub fn apply(self, input: &str, region_name_list: &Vec<String>) -> Option<(String, String)> {
-        match SequenceMatcher::get_most_similar_match(input, region_name_list, None) {
-            Ok(highest_match) => {
-                match tuple((
-                    is_not::<&str, &str, Error<&str>>("町村"),
-                    is_a("町村"),
-                    rest,
-                ))(input)
-                {
-                    Ok((_, separated)) => {
-                        let (_, _, rest) = separated;
-                        Some((rest.to_string(), highest_match))
-                    }
-                    Err(..) => None,
-                }
+        if let Ok(highest_match) =
+            SequenceMatcher::get_most_similar_match(input, region_name_list, None)
+        {
+            if let Ok((_, (_, _, rest))) = tuple((
+                is_not::<&str, &str, Error<&str>>("町村"),
+                is_a("町村"),
+                rest,
+            ))(input)
+            {
+                return Some((rest.to_string(), highest_match));
             }
-            Err(..) => None,
         }
+        None
     }
 }
 
