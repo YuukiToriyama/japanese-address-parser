dbf61454670634071220a9403059ff4433087d9c
ToriChan <github@toriyama.dev>
2024-04-28 21:35:40 +0900

    blocking parserのリファクタリング: ビルドターゲットがwasm*かつフィーチャフラグblockingを指定している場合にエラーメッセージを表示する (#252)

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,8 @@
+#[cfg(all(target_family = "wasm", feature = "blocking"))]
+compile_error! {
+    "The `blocking` feature is not supported with wasm target."
+}
+
 pub mod api;
 pub mod entity;
 mod err;
