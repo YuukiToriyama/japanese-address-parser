45b8bc2d7b25a7b0d13cc1616b2cc61486daedd9
Yuuki Toriyama <github@toriyama.dev>
2024-06-15 23:18:34 +0900

    update: #302: 「道穂」と「道穗」の表記ゆれに対応

--- core/src/parser/adapter/orthographical_variant_adapter.rs
+++ core/src/parser/adapter/orthographical_variant_adapter.rs
@@ -25,6 +25,7 @@ pub trait OrthographicalVariants {
     const 澤: Variant;
     const 塚: Variant;
     const 恵: Variant;
+    const 穂: Variant;
     const 梼: Variant;
 }
 
@@ -48,6 +49,7 @@ impl OrthographicalVariants for Variant {
     const 澤: Variant = &["澤", "沢"];
     const 塚: Variant = &["塚", "塚"];
     const 恵: Variant = &["恵", "惠"];
+    const 穂: Variant = &["穂", "穗"];
     const 梼: Variant = &["梼", "檮"];
 }
 
