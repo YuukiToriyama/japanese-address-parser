476b0759b5dad221caabc0e6e7d92a27a477d9ea
ToriChan <github@toriyama.dev>
2024-06-22 22:50:01 +0900

    郡名に「市」が含まれている場合に`trim_city_name()`が意図した挙動にならない問題を修正 (#312)
    * add: #268: `郡が省略されている場合への対応`に北海道余市郡・奈良県高市郡についてのケースを追加
    * fix: #268: 「余市郡」「高市郡」についても`trim_city_name()`がうまく動くように修正

--- core/src/util/trimmer.rs
+++ core/src/util/trimmer.rs
@@ -1,4 +1,9 @@
 pub fn trim_city_name(input: &str) -> String {
+    // 北海道高市郡と奈良県高市郡は郡名に「市」が入るので特例対応する
+    if input.starts_with("余市郡") || input.starts_with("高市郡") {
+        return input.chars().skip(3).collect::<String>();
+    }
+
     match input.chars().position(|c| c == '郡' || c == '市') {
         Some(position) => input.chars().skip(position + 1).collect::<String>(),
         None => input.to_string(),
@@ -11,8 +16,11 @@ mod tests {
 
     #[test]
     fn trim_city_name_成功() {
+        assert_eq!(trim_city_name("余市郡余市町"), "余市町");
+        assert_eq!(trim_city_name("余市郡仁木町"), "仁木町");
         assert_eq!(trim_city_name("南会津郡下郷町"), "下郷町");
         assert_eq!(trim_city_name("南会津郡只見町"), "只見町");
         assert_eq!(trim_city_name("白河市新白河一丁目"), "新白河一丁目");
+        assert_eq!(trim_city_name("高市郡明日香村"), "明日香村");
     }
 }
