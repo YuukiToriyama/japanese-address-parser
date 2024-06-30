3054e6e9c537a8060cdf59e12da9848d03aaf8ff
Yuuki Toriyama <github@toriyama.dev>
2024-06-30 22:13:04 +0900

    add: chimei-ruiju: 返り値の型を変更

--- repository/src/prefecture.rs
+++ repository/src/prefecture.rs
@@ -1,28 +1,42 @@
 use crate::entity::PrefectureMaster;
+use crate::error::ApiError;
 use jisx0401::Prefecture;
 
 pub struct PrefectureMasterRepository {}
 
 impl PrefectureMasterRepository {
-    pub async fn get(&self, prefecture: Prefecture) -> PrefectureMaster {
+    pub async fn get(&self, prefecture: &Prefecture) -> Result<PrefectureMaster, ApiError> {
         let url = format!(
             "https://{}.chimei-ruiju.org/master.json",
             prefecture.name_en()
         );
-        let response = reqwest::get(&url).await?;
-        let json = response.json::<PrefectureMaster>().await?;
-        json
+        let response = reqwest::get(&url)
+            .await
+            .map_err(|error| ApiError::Network {
+                url: url.clone(),
+                status_code: error.status().unwrap(),
+            })?;
+        let json = response
+            .json::<PrefectureMaster>()
+            .await
+            .map_err(|_| ApiError::Deserialize { url })?;
+        Ok(json)
     }
 }
 
 impl PrefectureMasterRepository {
-    pub fn get_blocking(&self, prefecture: Prefecture) -> PrefectureMaster {
+    pub fn get_blocking(&self, prefecture: Prefecture) -> Result<PrefectureMaster, ApiError> {
         let url = format!(
             "https://{}.chimei-ruiju.org/master.json",
             prefecture.name_en()
         );
-        let response = reqwest::blocking::get(&url)?;
-        let json = response.json::<PrefectureMaster>()?;
-        json
+        let response = reqwest::blocking::get(&url).map_err(|error| ApiError::Network {
+            url: url.clone(),
+            status_code: error.status().unwrap(),
+        })?;
+        let json = response
+            .json::<PrefectureMaster>()
+            .map_err(|_| ApiError::Deserialize { url })?;
+        Ok(json)
     }
 }
