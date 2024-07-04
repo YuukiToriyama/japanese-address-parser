1331a7668e48e5768a396df84c4fe5f82632bdc2
Yuuki Toriyama <github@toriyama.dev>
2024-07-04 21:29:00 +0900

    update: chimei-ruiju: `core/src/api/prefecture_master_api.rs`を`repository`クレートの`geolonia`モジュールに移動

--- repository/src/geolonia/prefecture_master_api.rs
+++ repository/src/geolonia/prefecture_master_api.rs
@@ -30,7 +30,7 @@ impl PrefectureMasterApi {
 
 #[cfg(all(test, not(feature = "blocking")))]
 mod tests {
-    use crate::api::prefecture_master_api::PrefectureMasterApi;
+    use crate::geolonia::prefecture_master_api::PrefectureMasterApi;
 
     #[tokio::test]
     async fn 非同期_富山県_成功() {
@@ -77,7 +77,7 @@ mod tests {
 
 #[cfg(all(test, feature = "blocking"))]
 mod blocking_tests {
-    use crate::api::prefecture_master_api::PrefectureMasterApi;
+    use crate::geolonia::prefecture_master_api::PrefectureMasterApi;
 
     #[test]
     fn 同期_富山県_成功() {
