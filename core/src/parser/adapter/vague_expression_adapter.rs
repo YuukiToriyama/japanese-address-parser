f63ed48e9a5e94e1790a38ed1ead6d3001fe93d4
yuuki toriyama <github@toriyama.dev>
2024-08-17 12:59:31 +0900

    update: #368: 市区町村名を丸ごと引数`with`で与えられた文字列で置き換えるのではなく、欠けている郡名のみを追加するように処理を修正
    引数`vague_address`に表記揺れが含まれる場合、`vague_address`の先頭と`with`の末尾が必ずしも一致しない場合があるため

--- core/src/parser/adapter/vague_expression_adapter.rs
+++ core/src/parser/adapter/vague_expression_adapter.rs
@@ -7,7 +7,7 @@ impl VagueExpressionAdapter {
         if let Ok(highest_match) =
             SequenceMatcher::get_most_similar_match(input, region_name_list, None)
         {
-            let complemented_address = complement_county_name(input, &highest_match);
+            let complemented_address = complement_county_name(input, &highest_match).unwrap();
             return Some((
                 highest_match.clone(),
                 complemented_address
@@ -20,21 +20,15 @@ impl VagueExpressionAdapter {
     }
 }
 
-fn complement_county_name(vague_address: &str, with: &str) -> String {
-    let mut vague_address = vague_address.to_string();
-    let mut concatenated_string = String::new();
-    for c in with.chars() {
-        match vague_address.chars().next() {
-            None => break,
-            Some(ch) => {
-                if c == ch {
-                    vague_address.remove(0);
-                }
-            }
-        };
-        concatenated_string.push(c);
+/// 郡名が抜けている住所に郡名を補う関数
+///
+/// 欠けている郡名を補うだけで、それ以上のことはしない。
+/// 市区町村名に表記揺れがあってもそれを上書きすることはしない。
+fn complement_county_name(vague_address: &str, with: &str) -> Result<String, &'static str> {
+    match with.chars().position(|c| c == '郡') {
+        None => Err("郡名が見つかりませんでした"),
+        Some(position) => Ok(with.chars().take(position + 1).collect::<String>() + vague_address),
     }
-    concatenated_string + vague_address.as_str()
 }
 
 #[cfg(test)]
@@ -47,17 +41,22 @@ mod tests {
     #[test]
     fn complement_county_name_郡名が省略されている場合() {
         assert_eq!(
-            complement_county_name("大町町大字福母297", "杵島郡大町町"),
+            complement_county_name("大町町大字福母297", "杵島郡大町町").unwrap(),
             "杵島郡大町町大字福母297"
         );
         assert_eq!(
-            complement_county_name("村田町大字村田字迫6", "柴田郡村田町"),
+            complement_county_name("村田町大字村田字迫6", "柴田郡村田町").unwrap(),
             "柴田郡村田町大字村田字迫6"
         );
         assert_eq!(
-            complement_county_name("玉村町上新田1116", "佐波郡玉村町"),
+            complement_county_name("玉村町上新田1116", "佐波郡玉村町").unwrap(),
             "佐波郡玉村町上新田1116"
         );
+        // 市区町村名に表記揺れも含む場合
+        assert_eq!(
+            complement_county_name("桧原村上元郷403", "西多摩郡檜原村").unwrap(),
+            "西多摩郡桧原村上元郷403"
+        )
     }
 
     #[test]
