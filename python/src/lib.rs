85fc00ae6107d7a38a7d6aaef042fcc657f97033
Yuuki Toriyama <github@toriyama.dev>
2024-08-22 01:12:56 +0900

    update: #379: 生成される型定義にコメントも含めるように設定

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -6,6 +6,18 @@ use pyo3::prelude::*;
 use pyo3_stub_gen::define_stub_info_gatherer;
 use pyo3_stub_gen::derive::{gen_stub_pyclass, gen_stub_pyfunction, gen_stub_pymethods};
 
+/// A class represent parse result.
+///
+/// パース処理の結果を表すクラスです。
+///
+/// - `address`: 都道府県名、市区町村名、町名、それ以降の文字列をそれぞれ格納する辞書型を返します。
+///     - `address.prefecture`: 都道府県名
+///     - `address.city`: 市区町村名
+///     - `address.town`: 町名
+///     - `address.rest`: それ以降
+/// - `error`: パース処理中にエラーが発生した場合、エラーのタイプとエラーメッセージを格納する辞書型を返します。
+///     - `error_type`: エラータイプ
+///     - `error_message`: エラーメッセージ
 #[gen_stub_pyclass]
 #[pyclass(name = "ParseResult")]
 struct PyParseResult {
@@ -40,6 +52,12 @@ struct PyParser {
 #[gen_stub_pymethods]
 #[pymethods]
 impl PyParser {
+    /// Construct a parser.
+    ///
+    /// パーサーを生成します。
+    ///
+    /// :return: パーサー
+    /// :rtype: Parser
     #[new]
     fn default() -> Self {
         PyParser {
@@ -47,11 +65,25 @@ impl PyParser {
         }
     }
 
+    /// Format informal address into formal style
+    ///
+    /// 入力された住所を正式な表記に整形します。
+    ///
+    /// :param str address: 住所の文字列
+    /// :return: 整形、分割済みの住所の構造体
+    /// :rtype: ParseResult
     fn parse(&self, address: &str) -> PyParseResult {
         self.parser.parse_blocking(address).into()
     }
 }
 
+/// Format informal address into formal style
+///
+/// 入力された住所を正式な表記に整形します。
+///
+/// :param str address: 住所の文字列
+/// :return: 整形、分割済みの住所の構造体
+/// :rtype: ParseResult
 #[gen_stub_pyfunction]
 #[pyfunction]
 fn parse(address: &str) -> PyParseResult {
