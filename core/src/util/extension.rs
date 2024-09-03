e04e8a24025db83de17966ede51d8a330de672d1
Yuuki Toriyama <github@toriyama.dev>
2024-09-03 23:19:40 +0900

    add: #400: 文字列から異字体セレクタを取り除くメソッドを定義

--- core/src/util/extension.rs
+++ core/src/util/extension.rs
@@ -9,9 +9,22 @@ impl CharExt for char {
     }
 }
 
+pub(crate) trait StrExt {
+    fn strip_variation_selectors(&self) -> String;
+}
+
+impl StrExt for str {
+    /// 文字列から異字体セレクタを取り除きます
+    fn strip_variation_selectors(&self) -> String {
+        self.chars()
+            .filter(|c| !c.is_variation_selector())
+            .collect()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::util::extension::CharExt;
+    use crate::util::extension::{CharExt, StrExt};
 
     #[test]
     fn is_variation_selector() {
@@ -29,5 +42,21 @@ mod tests {
 
         assert_eq!('\u{E01EF}'.is_variation_selector(), true);
         assert_eq!('\u{E01F0}'.is_variation_selector(), false);
+    }
+
+    #[test]
+    fn strip_variation_selectors_逢坂() {
+        let normal = "\u{9022}\u{5742}"; // 逢坂
+        let variant = "\u{9022}\u{E0101}\u{5742}"; // 逢󠄁坂
+        assert_ne!(normal, variant);
+        assert_eq!(normal, variant.strip_variation_selectors());
+    }
+
+    #[test]
+    fn strip_variation_selectors_茨城() {
+        let normal = "\u{8328}\u{57CE}";
+        let variant = "\u{8328}\u{E0100}\u{57CE}";
+        assert_ne!(normal, variant);
+        assert_eq!(normal, variant.strip_variation_selectors());
     }
 }
