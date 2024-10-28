239b3797f874646b49bdce0f1bf7c26c175d0bd5
Yuuki Toriyama <github@toriyama.dev>
2024-10-28 21:43:03 +0900

    update: #470: `experimental`モジュールもdocs.rsに反映されるように設定
    c.f. https://users.rust-lang.org/t/how-to-document-optional-features-in-api-docs/64577

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,6 +7,7 @@
 //! - `eliminate-whitespaces`*(experimental)*: Enable elimination of whitespaces from given text
 //! - `experimental`: Enable experimental module
 
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #[cfg(all(target_family = "wasm", feature = "blocking"))]
 compile_error! {
     "The `blocking` feature is not supported with wasm target."
@@ -17,6 +18,7 @@ pub(crate) mod domain;
 #[deprecated(since = "0.1.6", note = "This module will be deleted in v0.2")]
 pub mod entity;
 #[cfg(feature = "experimental")]
+#[cfg_attr(docsrs, doc(cfg(feature = "experimental")))]
 pub mod experimental;
 mod formatter;
 pub mod parser;
