1dfedb0aa8a93308be161d29f9cab13a4d53b912
Yuuki Toriyama <github@toriyama.dev>
2024-11-04 15:00:53 +0900

    add: #499: 同期版の関数を追加

--- core/src/interactor/geolonia.rs
+++ core/src/interactor/geolonia.rs
@@ -5,10 +5,23 @@ use crate::domain::geolonia::error::Error;
 
 #[allow(dead_code)]
 pub(crate) trait GeoloniaInteractor {
-    /// 都道府県マスタを取得
+    /// 都道府県マスタを取得(非同期)
     async fn get_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error>;
-    /// 市区町村マスタを取得
+
+    /// 都道府県マスタを取得(同期)
+    #[cfg(feature = "blocking")]
+    fn get_blocking_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error>;
+
+    /// 市区町村マスタを取得(非同期)
     async fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error>;
+
+    /// 市区町村マスタを取得(同期)
+    #[cfg(feature = "blocking")]
+    fn get_blocking_city_master(
+        &self,
+        prefecture_name: &str,
+        city_name: &str,
+    ) -> Result<City, Error>;
 }
 
 #[allow(dead_code)]
@@ -20,8 +33,24 @@ impl GeoloniaInteractor for GeoloniaInteractorImpl {
         prefecture_master_api.get(prefecture_name).await
     }
 
+    #[cfg(feature = "blocking")]
+    fn get_blocking_prefecture_master(&self, prefecture_name: &str) -> Result<Prefecture, Error> {
+        let prefecture_master_api = PrefectureMasterApi::default();
+        prefecture_master_api.get_blocking(prefecture_name)
+    }
+
     async fn get_city_master(&self, prefecture_name: &str, city_name: &str) -> Result<City, Error> {
         let city_master_api = CityMasterApi::default();
         city_master_api.get(prefecture_name, city_name).await
+    }
+
+    #[cfg(feature = "blocking")]
+    fn get_blocking_city_master(
+        &self,
+        prefecture_name: &str,
+        city_name: &str,
+    ) -> Result<City, Error> {
+        let city_master_api = CityMasterApi::default();
+        city_master_api.get_blocking(prefecture_name, city_name)
     }
 }
