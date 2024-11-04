0618a2104fc095a9a0fb0173eb4c5514f5e2f7ec
ToriChan <github@toriyama.dev>
2024-11-04 16:57:43 +0900

    apiモジュールを非推奨化: `parser::Parser`の実装を`GeoloniaInteractor`ベースに書き換え (#506)
    * update: #499: `parser`モジュールの`parse()`および`parse_blocking()`をDeprecatedに指定
    * update: #499: `Parser#parse`と`Parser#parse_blocking`の実装を`interactor::geolonia::GeoloniaInteractor`ベースに書き換え
    * update: #499: `parse()`および`parse_blocking()`に対するテストコードを`Parser`に対するテストに書き換え

--- core/src/parser.rs
+++ core/src/parser.rs
@@ -6,6 +6,7 @@ use crate::api::BlockingApi;
 use crate::domain::common::token::Token;
 use crate::domain::geolonia::entity::Address;
 use crate::domain::geolonia::error::{Error, ParseErrorKind};
+use crate::interactor::geolonia::{GeoloniaInteractor, GeoloniaInteractorImpl};
 use crate::tokenizer::{End, Tokenizer};
 use serde::Serialize;
 
@@ -39,25 +40,14 @@ impl From<Tokenizer<End>> for Address {
 /// }
 /// ```
 pub struct Parser {
-    async_api: Arc<AsyncApi>,
-    #[cfg(feature = "blocking")]
-    blocking_api: Arc<BlockingApi>,
+    interactor: Arc<GeoloniaInteractorImpl>,
 }
 
 impl Default for Parser {
     /// Constructs a new `Parser`.
-    #[cfg(feature = "blocking")]
     fn default() -> Self {
         Self {
-            async_api: Arc::new(Default::default()),
-            blocking_api: Arc::new(Default::default()),
-        }
-    }
-    /// Constructs a new `Parser`.
-    #[cfg(not(feature = "blocking"))]
-    fn default() -> Self {
-        Self {
-            async_api: Arc::new(Default::default()),
+            interactor: Arc::new(Default::default()),
         }
     }
 }
@@ -65,19 +55,144 @@ impl Default for Parser {
 impl Parser {
     /// Parses the given `address` asynchronously.
     pub async fn parse(&self, address: &str) -> ParseResult {
-        parse(self.async_api.clone(), address).await
+        let interasctor = self.interactor.clone();
+        let tokenizer = Tokenizer::new(address);
+        // 都道府県を特定
+        let (prefecture, tokenizer) = match tokenizer.read_prefecture() {
+            Ok(found) => found,
+            Err(tokenizer) => {
+                return ParseResult {
+                    address: Address::from(tokenizer),
+                    error: Some(Error::new_parse_error(ParseErrorKind::Prefecture)),
+                }
+            }
+        };
+        // その都道府県の市町村名リストを取得
+        let prefecture_master = match interasctor
+            .get_prefecture_master(prefecture.name_ja())
+            .await
+        {
+            Err(error) => {
+                return ParseResult {
+                    address: Address::from(tokenizer.finish()),
+                    error: Some(error),
+                };
+            }
+            Ok(result) => result,
+        };
+        // 市町村名を特定
+        let (city_name, tokenizer) = match tokenizer.read_city(&prefecture_master.cities) {
+            Ok(found) => found,
+            Err(not_found) => {
+                // 市区町村が特定できない場合かつフィーチャフラグが有効な場合、郡名が抜けている可能性を検討
+                match not_found.read_city_with_county_name_completion(&prefecture_master.cities) {
+                    Ok(found) if cfg!(feature = "city-name-correction") => found,
+                    _ => {
+                        // それでも見つからない場合は終了
+                        return ParseResult {
+                            address: Address::from(tokenizer.finish()),
+                            error: Some(Error::new_parse_error(ParseErrorKind::City)),
+                        };
+                    }
+                }
+            }
+        };
+        // その市町村の町名リストを取得
+        let city = match interasctor
+            .get_city_master(prefecture.name_ja(), &city_name)
+            .await
+        {
+            Err(error) => {
+                return ParseResult {
+                    address: Address::from(tokenizer.finish()),
+                    error: Some(error),
+                };
+            }
+            Ok(result) => result,
+        };
+        // 町名を特定
+        let Ok((_, tokenizer)) =
+            tokenizer.read_town(city.towns.iter().map(|x| x.name.clone()).collect())
+        else {
+            return ParseResult {
+                address: Address::from(tokenizer.finish()),
+                error: Some(Error::new_parse_error(ParseErrorKind::Town)),
+            };
+        };
+
+        ParseResult {
+            address: Address::from(tokenizer.finish()),
+            error: None,
+        }
     }
 
     /// Parses the given `address` synchronously.
     #[cfg(feature = "blocking")]
     pub fn parse_blocking(&self, address: &str) -> ParseResult {
-        parse_blocking(self.blocking_api.clone(), address)
+        let interactor = self.interactor.clone();
+        let tokenizer = Tokenizer::new(address);
+        let (prefecture, tokenizer) = match tokenizer.read_prefecture() {
+            Ok(found) => found,
+            Err(tokenizer) => {
+                return ParseResult {
+                    address: Address::from(tokenizer),
+                    error: Some(Error::new_parse_error(ParseErrorKind::Prefecture)),
+                }
+            }
+        };
+        let prefecture_master =
+            match interactor.get_blocking_prefecture_master(prefecture.name_ja()) {
+                Err(error) => {
+                    return ParseResult {
+                        address: Address::from(tokenizer.finish()),
+                        error: Some(error),
+                    };
+                }
+                Ok(result) => result,
+            };
+        let (city_name, tokenizer) = match tokenizer.read_city(&prefecture_master.cities) {
+            Ok(found) => found,
+            Err(not_found) => {
+                match not_found.read_city_with_county_name_completion(&prefecture_master.cities) {
+                    Ok(found) if cfg!(feature = "city-name-correction") => found,
+                    _ => {
+                        return ParseResult {
+                            address: Address::from(tokenizer.finish()),
+                            error: Some(Error::new_parse_error(ParseErrorKind::City)),
+                        };
+                    }
+                }
+            }
+        };
+        let city = match interactor.get_blocking_city_master(prefecture.name_ja(), &city_name) {
+            Err(error) => {
+                return ParseResult {
+                    address: Address::from(tokenizer.finish()),
+                    error: Some(error),
+                };
+            }
+            Ok(result) => result,
+        };
+        let Ok((_, tokenizer)) =
+            tokenizer.read_town(city.towns.iter().map(|x| x.name.clone()).collect())
+        else {
+            return ParseResult {
+                address: Address::from(tokenizer.finish()),
+                error: Some(Error::new_parse_error(ParseErrorKind::Town)),
+            };
+        };
+
+        ParseResult {
+            address: Address::from(tokenizer.finish()),
+            error: None,
+        }
     }
 }
 
 /// A function to parse the given address asynchronously.
 ///
 /// publicにしていますが、直接の使用は推奨されません。[Parser]の利用を検討してください。
+#[deprecated(since = "0.1.23", note = "This module will be deleted in v0.2")]
 pub async fn parse(api: Arc<AsyncApi>, input: &str) -> ParseResult {
     let tokenizer = Tokenizer::new(input);
     // 都道府県を特定
@@ -145,17 +260,14 @@ pub async fn parse(api: Arc<AsyncApi>, input: &str) -> ParseResult {
 
 #[cfg(all(test, not(feature = "blocking")))]
 mod tests {
-    use crate::api::AsyncApi;
     use crate::domain::geolonia::error::ParseErrorKind;
-    use crate::parser::parse;
-    use crate::repository::geolonia::city_master_api::CityMasterApi;
-    use crate::repository::geolonia::prefecture_master_api::PrefectureMasterApi;
+    use crate::parser::Parser;
     use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
     #[tokio::test]
     async fn 都道府県名が誤っている場合() {
-        let api: AsyncApi = Default::default();
-        let result = parse(api.into(), "青盛県青森市長島１丁目１−１").await;
+        let parser = Parser::default();
+        let result = parser.parse("青盛県青森市長島１丁目１−１").await;
         assert_eq!(result.address.prefecture, "");
         assert_eq!(result.address.city, "");
         assert_eq!(result.address.town, "");
@@ -168,24 +280,9 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn 都道府県マスタが取得できない場合() {
-        let mut api: AsyncApi = Default::default();
-        api.prefecture_master_api = PrefectureMasterApi {
-            server_url: "https://example.com/invalid_url/api/",
-        };
-
-        let result = parse(api.into(), "青森県青森市長島１丁目１−１").await;
-        assert_eq!(result.error.is_some(), true);
-        assert_eq!(result.address.prefecture, "青森県");
-        assert_eq!(result.address.city, "");
-        assert_eq!(result.address.town, "");
-        assert_eq!(result.address.rest, "青森市長島１丁目１−１");
-    }
-
-    #[tokio::test]
     async fn 市区町村名が誤っている場合() {
-        let api: AsyncApi = Default::default();
-        let result = parse(api.into(), "青森県青盛市長島１丁目１−１").await;
+        let parser = Parser::default();
+        let result = parser.parse("青森県青盛市長島１丁目１−１").await;
         assert_eq!(result.address.prefecture, "青森県");
         assert_eq!(result.address.city, "");
         assert_eq!(result.address.town, "");
@@ -198,24 +295,9 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn 市区町村マスタが取得できない場合() {
-        let mut api: AsyncApi = Default::default();
-        api.city_master_api = CityMasterApi {
-            server_url: "https://example.com/invalid_url/api/",
-        };
-
-        let result = parse(api.into(), "青森県青森市長島１丁目１−１").await;
-        assert_eq!(result.error.is_some(), true);
-        assert_eq!(result.address.prefecture, "青森県");
-        assert_eq!(result.address.city, "青森市");
-        assert_eq!(result.address.town, "");
-        assert_eq!(result.address.rest, "長島１丁目１−１");
-    }
-
-    #[tokio::test]
     async fn 町名が誤っている場合() {
-        let api: AsyncApi = Default::default();
-        let result = parse(api.into(), "青森県青森市永嶋１丁目１−１").await;
+        let parser = Parser::default();
+        let result = parser.parse("青森県青森市永嶋１丁目１−１").await;
         assert_eq!(result.address.prefecture, "青森県");
         assert_eq!(result.address.city, "青森市");
         assert_eq!(result.address.town, "");
@@ -231,8 +313,8 @@ mod tests {
 
     #[wasm_bindgen_test]
     async fn parse_wasm_success() {
-        let api: AsyncApi = Default::default();
-        let result = parse(api.into(), "兵庫県淡路市生穂新島8番地").await;
+        let parser = Parser::default();
+        let result = parser.parse("兵庫県淡路市生穂新島8番地").await;
         assert_eq!(result.address.prefecture, "兵庫県".to_string());
         assert_eq!(result.address.city, "淡路市".to_string());
         assert_eq!(result.address.town, "生穂".to_string());
@@ -245,6 +327,7 @@ mod tests {
 ///
 /// publicにしていますが、直接の使用は推奨されません。[Parser]の利用を検討してください。
 #[cfg(feature = "blocking")]
+#[deprecated(since = "0.1.23", note = "This module will be deleted in v0.2")]
 pub fn parse_blocking(api: Arc<BlockingApi>, input: &str) -> ParseResult {
     let tokenizer = Tokenizer::new(input);
     let (prefecture, tokenizer) = match tokenizer.read_prefecture() {
@@ -305,14 +388,13 @@ pub fn parse_blocking(api: Arc<BlockingApi>, input: &str) -> ParseResult {
 
 #[cfg(all(test, feature = "blocking"))]
 mod blocking_tests {
-    use crate::api::BlockingApi;
     use crate::domain::geolonia::error::ParseErrorKind;
-    use crate::parser::parse_blocking;
+    use crate::parser::Parser;
 
     #[test]
     fn parse_blocking_success_埼玉県秩父市熊木町8番15号() {
-        let client: BlockingApi = Default::default();
-        let result = parse_blocking(client.into(), "埼玉県秩父市熊木町8番15号");
+        let parser = Parser::default();
+        let result = parser.parse_blocking("埼玉県秩父市熊木町8番15号");
         assert_eq!(result.address.prefecture, "埼玉県");
         assert_eq!(result.address.city, "秩父市");
         assert_eq!(result.address.town, "熊木町");
@@ -322,8 +404,8 @@ mod blocking_tests {
 
     #[test]
     fn parse_blocking_fail_市町村名が間違っている場合() {
-        let client: BlockingApi = Default::default();
-        let result = parse_blocking(client.into(), "埼玉県秩父柿熊木町8番15号");
+        let parser = Parser::default();
+        let result = parser.parse_blocking("埼玉県秩父柿熊木町8番15号");
         assert_eq!(result.address.prefecture, "埼玉県");
         assert_eq!(result.address.city, "");
         assert_eq!(result.address.town, "");
