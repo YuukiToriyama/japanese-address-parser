ad8b07dac6764faec363c35422611da73a7e1776
Yuuki Toriyama <github@toriyama.dev>
2024-11-04 16:59:40 +0900

    update: #499: `repository::geolonia::city_master_api`モジュールを非推奨化
    v0.2で廃止予定

--- core/src/repository/geolonia.rs
+++ core/src/repository/geolonia.rs
@@ -1,4 +1,5 @@
 pub(crate) mod city;
+#[deprecated(since = "0.1.23", note = "This module will be deleted in v0.2")]
 pub mod city_master_api;
 pub(crate) mod prefecture;
 pub mod prefecture_master_api;
