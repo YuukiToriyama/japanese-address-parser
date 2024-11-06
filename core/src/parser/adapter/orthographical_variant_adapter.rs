c8f57f61b546d564f0f2771e1d50bb705043e0c7
Yuuki Toriyama <github@toriyama.dev>
2024-11-06 23:30:55 +0900

    update: 表記揺れアダプタのリファクタ: 表記揺れのパターンの型を`&'static [&'static str]`から`&[char]`に変更
    `&str`だと1文字以上のものも定義できてしまうので`char`にすることで1文字以上のものが入れられないようにしたい

--- core/src/parser/adapter/orthographical_variant_adapter.rs
+++ core/src/parser/adapter/orthographical_variant_adapter.rs
@@ -33,36 +33,36 @@ pub enum OrthographicalVariant {
 }
 
 impl OrthographicalVariant {
-    fn value(&self) -> &'static [&'static str] {
+    fn value(&self) -> &[char] {
         match self {
-            OrthographicalVariant::の => &["の", "ノ", "之"],
-            OrthographicalVariant::ツ => &["ツ", "ッ"],
-            OrthographicalVariant::ケ => &["ケ", "ヶ", "が", "ガ"],
-            OrthographicalVariant::薮 => &["薮", "藪", "籔"],
-            OrthographicalVariant::崎 => &["崎", "﨑"],
-            OrthographicalVariant::檜 => &["桧", "檜"],
-            OrthographicalVariant::龍 => &["龍", "竜"],
-            OrthographicalVariant::竈 => &["竈", "竃", "釜"],
-            OrthographicalVariant::嶋 => &["嶋", "島"],
-            OrthographicalVariant::舘 => &["舘", "館"],
-            OrthographicalVariant::脊 => &["脊", "背"],
-            OrthographicalVariant::渕 => &["渕", "淵"],
-            OrthographicalVariant::己 => &["己", "巳"],
-            OrthographicalVariant::槇 => &["槇", "槙"],
-            OrthographicalVariant::治 => &["治", "冶"],
-            OrthographicalVariant::佛 => &["佛", "仏"],
-            OrthographicalVariant::澤 => &["澤", "沢"],
-            OrthographicalVariant::塚 => &["塚", "塚"],
-            OrthographicalVariant::恵 => &["恵", "惠"],
-            OrthographicalVariant::穂 => &["穂", "穗"],
-            OrthographicalVariant::梼 => &["梼", "檮"],
-            OrthographicalVariant::蛍 => &["蛍", "螢"],
-            OrthographicalVariant::與 => &["與", "与"],
-            OrthographicalVariant::瀧 => &["瀧", "滝"],
-            OrthographicalVariant::籠 => &["籠", "篭"],
-            OrthographicalVariant::濱 => &["濱", "浜"],
-            OrthographicalVariant::祗 => &["祗", "祇"],
-            OrthographicalVariant::曾 => &["曾", "曽"],
+            OrthographicalVariant::の => &['の', 'ノ', '之'],
+            OrthographicalVariant::ツ => &['ツ', 'ッ'],
+            OrthographicalVariant::ケ => &['ケ', 'ヶ', 'が', 'ガ'],
+            OrthographicalVariant::薮 => &['薮', '藪', '籔'],
+            OrthographicalVariant::崎 => &['崎', '﨑'],
+            OrthographicalVariant::檜 => &['桧', '檜'],
+            OrthographicalVariant::龍 => &['龍', '竜'],
+            OrthographicalVariant::竈 => &['竈', '竃', '釜'],
+            OrthographicalVariant::嶋 => &['嶋', '島'],
+            OrthographicalVariant::舘 => &['舘', '館'],
+            OrthographicalVariant::脊 => &['脊', '背'],
+            OrthographicalVariant::渕 => &['渕', '淵'],
+            OrthographicalVariant::己 => &['己', '巳'],
+            OrthographicalVariant::槇 => &['槇', '槙'],
+            OrthographicalVariant::治 => &['治', '冶'],
+            OrthographicalVariant::佛 => &['佛', '仏'],
+            OrthographicalVariant::澤 => &['澤', '沢'],
+            OrthographicalVariant::塚 => &['塚', '塚'],
+            OrthographicalVariant::恵 => &['恵', '惠'],
+            OrthographicalVariant::穂 => &['穂', '穗'],
+            OrthographicalVariant::梼 => &['梼', '檮'],
+            OrthographicalVariant::蛍 => &['蛍', '螢'],
+            OrthographicalVariant::與 => &['與', '与'],
+            OrthographicalVariant::瀧 => &['瀧', '滝'],
+            OrthographicalVariant::籠 => &['籠', '篭'],
+            OrthographicalVariant::濱 => &['濱', '浜'],
+            OrthographicalVariant::祗 => &['祗', '祇'],
+            OrthographicalVariant::曾 => &['曾', '曽'],
         }
     }
 }
@@ -77,7 +77,7 @@ impl OrthographicalVariantAdapter {
         let variant_list: Vec<&OrthographicalVariant> = self
             .variant_list
             .iter()
-            .filter(|v| v.value().iter().any(|c| input.contains(c)))
+            .filter(|v| v.value().iter().any(|&c| input.contains(c)))
             .collect();
         if variant_list.is_empty() {
             return None;
@@ -91,9 +91,10 @@ impl OrthographicalVariantAdapter {
             // variantから順列を作成
             // ["ケ", "ヶ", "が"] -> (ケ, ヶ), (ケ, が), (ヶ, ケ), (ヶ, が), (が, ケ), (が, ヶ)
             for permutation in variant.value().iter().permutations(2) {
-                for candidate in candidates.iter().filter(|c| c.contains(permutation[0])) {
+                let (&a, &b) = (permutation[0], permutation[1]);
+                for candidate in candidates.iter().filter(|c| c.contains(a)) {
                     // マッチ候補の中でパターンに引っかかるものがあれば文字を置き換えてマッチを試す
-                    let edited_region_name = candidate.replace(permutation[0], permutation[1]);
+                    let edited_region_name = modify_specific_character(candidate, a, b);
                     if input.starts_with(&edited_region_name) {
                         // マッチすれば早期リターン
                         return Some((
@@ -114,4 +115,10 @@ impl OrthographicalVariantAdapter {
         }
         None
     }
+}
+
+fn modify_specific_character(text: &str, from: char, to: char) -> String {
+    text.chars()
+        .map(|x| if x == from { to } else { x })
+        .collect()
 }
