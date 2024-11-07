0d07649b72b1dfb1bcfa33e8a0a987dbdfb14d10
Yuuki Toriyama <github@toriyama.dev>
2024-11-07 23:30:31 +0900

    remove: 表記揺れアダプタのリファクタ: ベンチマークを削除
    表記揺れアダプタのモジュールをprivateに変更するのでベンチマークを走らせることができなくなるため

--- core/benches/core_benchmark.rs
+++
@@ -1,7 +0,0 @@
-mod orthographical_variant_adapter;
-
-use crate::orthographical_variant_adapter::bench_orthographical_variant_adapter;
-use criterion::{criterion_group, criterion_main};
-
-criterion_group!(benches, bench_orthographical_variant_adapter);
-criterion_main!(benches);
