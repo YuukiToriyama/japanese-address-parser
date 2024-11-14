5a01a0f771af2e9b5ff513e8027a802be84485d2
Yuuki Toriyama <github@toriyama.dev>
2024-11-14 23:41:59 +0900

    delete: #473: `core::repository::city_master_api`モジュールを削除

--- core/src/repository/geolonia.rs
+++ core/src/repository/geolonia.rs
@@ -1,6 +1,4 @@
 pub(crate) mod city;
-#[deprecated(since = "0.1.23", note = "This module will be deleted in v0.2")]
-pub mod city_master_api;
 pub(crate) mod prefecture;
 #[deprecated(since = "0.1.23", note = "This module will be deleted in v0.2")]
 pub mod prefecture_master_api;
