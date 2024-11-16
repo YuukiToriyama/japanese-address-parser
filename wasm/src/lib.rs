368c4270dba3d831d478037abff5f50c90076016
Yuuki Toriyama <github@toriyama.dev>
2024-11-16 16:59:56 +0900

    update: #535: `#[wasm_bindgen(start)] fn start()`の定義箇所を`nightly.rs`から`lib.rs`に移動

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,6 +5,12 @@ use japanese_address_parser::parser;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsValue;
 
+#[wasm_bindgen(start)]
+fn start() {
+    #[cfg(feature = "nightly")]
+    console_log::init_with_level(log::Level::Trace).expect("could not initialize log");
+}
+
 #[wasm_bindgen(typescript_custom_section)]
 const TYPESCRIPT_TYPE: &'static str = r#"
 export interface ParseResult {
