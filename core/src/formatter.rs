f4bf2738b97f0a000a4b31e7f8d19797afc2554e
Yuuki Toriyama <github@toriyama.dev>
2025-05-03 18:25:36 +0900

    add: #450: 文字列中に含まれる半角カタカナを全角カタカナに変換する関数を定義

--- core/src/formatter.rs
+++ core/src/formatter.rs
@@ -1,4 +1,5 @@
 pub(crate) mod chome_with_arabic_numerals;
 pub(crate) mod fullwidth_character;
+pub(crate) mod halfwidth_character;
 pub(crate) mod house_number;
 pub(crate) mod informal_town_name_notation;
