916209e15f63702fd61857e8885a9e79f9b607fb
Yuuki Toriyama <github@toriyama.dev>
2026-05-08 12:08:55 +0900

    add: #684: 住所を一つ受け取り処理を行なうツール関数を実装

--- mcp/src/main.rs
+++ mcp/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use crate::server::ParseAddressServer;
+use rmcp::ServiceExt;
+use rmcp::transport::stdio;
+
+mod server;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let server = ParseAddressServer::new();
+    let service = server.serve(stdio()).await?;
+    service.waiting().await?;
+    Ok(())
+}
---
+++ mcp/src/server.rs
@@ -0,0 +1,41 @@
+mod process_an_address;
+
+use rmcp::handler::server::tool::ToolRouter;
+use rmcp::handler::server::wrapper::Parameters;
+use rmcp::model::{CallToolResult, Implementation, ServerCapabilities, ServerInfo};
+use rmcp::{ErrorData, ServerHandler, tool, tool_handler, tool_router};
+
+#[derive(Clone)]
+pub(crate) struct ParseAddressServer {
+    #[allow(dead_code)]
+    tool_router: ToolRouter<Self>,
+}
+
+#[tool_router]
+impl ParseAddressServer {
+    pub fn new() -> Self {
+        Self {
+            tool_router: Self::tool_router(),
+        }
+    }
+
+    #[tool(description = "日本の住所を都道府県・市区町村・町名・それ以降に分割できます")]
+    async fn process_an_address(
+        &self,
+        Parameters(params): Parameters<process_an_address::RequestParameters>,
+    ) -> Result<CallToolResult, ErrorData> {
+        process_an_address::process_an_address(params).await
+    }
+}
+
+#[tool_handler]
+impl ServerHandler for ParseAddressServer {
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo::new(ServerCapabilities::builder().enable_tools().build())
+            .with_server_info(Implementation::from_build_env())
+            .with_instructions(
+                "日本の住所を都道府県・市区町村・町名・それ以降に分割できるMCPサーバーです。 \
+                process_an_address: 1件の住所を解析",
+            )
+    }
+}
